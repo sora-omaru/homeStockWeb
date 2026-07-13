6ad4b79ca1b41dd13f841a20b48864a8be52a817
sora-omaru <konggongxia317@gmail.com>
2026-07-13 16:43:41 +0900

    feat:ログイン時にitemページへ遷移するように設定

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,23 +4,28 @@ import { getMe, login } from "@/lib/api/auth/auth";
 import Link from "next/link";
 import { FormEvent, useState } from "react";
 import styles from "./page.module.scss";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
   async function handleLogin(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     await login({ email, password });
-    const me = await getMe();
-    console.log(me);
+    await getMe();
+
+    router.push("/items");
   }
 
   return (
     <main className={styles.main}>
       <section className={styles.card}>
-        <Link href="/" className={styles.backLink}>← Home Stock</Link>
+        <Link href="/" className={styles.backLink}>
+          ← Home Stock
+        </Link>
         <p className={styles.eyebrow}>WELCOME BACK</p>
         <h1 className={styles.title}>ログイン</h1>
         <p className={styles.description}>おうちのストックを確認しましょう。</p>
@@ -28,11 +33,27 @@ export default function LoginPage() {
         <form className={styles.form} onSubmit={handleLogin}>
           <label className={styles.field}>
             メールアドレス
-            <input className={styles.input} type="email" value={email} placeholder="name@example.com" autoComplete="email" required onChange={(event) => setEmail(event.target.value)} />
+            <input
+              className={styles.input}
+              type="email"
+              value={email}
+              placeholder="name@example.com"
+              autoComplete="email"
+              required
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </label>
           <label className={styles.field}>
             パスワード
-            <input className={styles.input} type="password" value={password} placeholder="パスワードを入力" autoComplete="current-password" required onChange={(event) => setPassword(event.target.value)} />
+            <input
+              className={styles.input}
+              type="password"
+              value={password}
+              placeholder="パスワードを入力"
+              autoComplete="current-password"
+              required
+              onChange={(event) => setPassword(event.target.value)}
+            />
           </label>
           <button className={styles.submit} type="submit">
             ログイン

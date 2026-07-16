73a845aabd589a7255b9ad5b9cd4fa401822af7f
sora-omaru <konggongxia317@gmail.com>
2026-07-16 14:42:11 +0900

    feat:バックエンドが想定しないエラーもキャッチできるように変更

--- src/lib/api/client.ts
+++ src/lib/api/client.ts
@@ -1,4 +1,5 @@
 import { env } from "@/lib/env";
+import { ApiError, ApiErrorResponse } from "./error/api-error";
 
 type ApiClientOptions = {
   method?: "GET" | "POST" | "PUT" | "DELETE";
@@ -20,5 +21,29 @@ export async function apiClient<TResponse>(
     body: options.body ? JSON.stringify(options.body) : undefined,
     signal: options.signal,
   });
+
+  if (!response.ok) {
+    //バックエンドが想定しないエラーもキャッチできるよう変更
+    let errorResponse: ApiErrorResponse;
+    try {
+      errorResponse = (await response.json()) as ApiErrorResponse;
+    } catch {
+      errorResponse = {
+        code: "UNKNOWN_ERROR",
+        message: "通信処理中にエラーが発生しました",
+      };
+    }
+
+    // const errorResponse = (await response.json()) as ApiErrorResponse;
+
+    throw new ApiError(
+      response.status,
+      errorResponse.code,
+      errorResponse.message,
+    );
+  }
+  if (response.status === 204) {
+    return undefined as TResponse;
+  }
   return response.json() as Promise<TResponse>;
 }

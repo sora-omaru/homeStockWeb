8bcc18c25f5c3e219859b9e9afa4607771b92b43
sora-omaru <konggongxia317@gmail.com>
2026-07-17 09:40:21 +0900

    feat:createItem作成

--- src/lib/api/item.ts
+++ src/lib/api/item.ts
@@ -1,4 +1,8 @@
-import { ItemResponse, UpdateItemRequest } from "@/types/item";
+import {
+  ItemCreateRequest,
+  ItemResponse,
+  UpdateItemRequest,
+} from "@/types/item";
 import { apiClient } from "./client";
 
 export async function getItems(): Promise<ItemResponse[]> {
@@ -12,13 +16,21 @@ export async function getItem(
   return apiClient<ItemResponse>(`/api/v1/items/${id}`, { signal });
 }
 
-
 export async function updateItem(
   itemId: number,
   request: UpdateItemRequest,
 ): Promise<ItemResponse> {
   return apiClient<ItemResponse>(`/api/v1/items/${itemId}`, {
     method: "PUT",
+    body: request,
+  });
+}
+
+export async function createItem(
+  request: ItemCreateRequest,
+): Promise<ItemResponse> {
+  return apiClient<ItemResponse>(`/api/v1/items`, {
+    method: "POST",
     body: request,
   });
 }
--- src/types/item.ts
+++ src/types/item.ts
@@ -1,18 +1,28 @@
 import { ItemCategory } from "./item-category";
 
 export type ItemResponse = {
-    id :number;
-    name: string;
-    quantity:number;
-    minQuantity:number;
-    category:ItemCategory;
-    locationId:number | null;
-    locationName:string | null;
-    expirationDate:string|null;
-    memo:string|null;
+  id: number;
+  name: string;
+  quantity: number;
+  minQuantity: number;
+  category: ItemCategory;
+  locationId: number | null;
+  locationName: string | null;
+  expirationDate: string | null;
+  memo: string | null;
 };
 
 export type UpdateItemRequest = {
+  name: string;
+  quantity: number;
+  minQuantity: number;
+  category: ItemCategory;
+  locationId: number | null;
+  expirationDate: string | null;
+  memo: string | null;
+};
+
+export type ItemCreateRequest = {
   name: string;
   quantity: number;
   minQuantity: number;

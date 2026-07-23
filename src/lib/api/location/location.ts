7a1b282c52b74b22546a53ca73bb3ba17f7dd804
sora-omaru <konggongxia317@gmail.com>
2026-07-23 09:33:47 +0900

    feat:createLocation作成

--- src/lib/api/location/location.ts
+++ src/lib/api/location/location.ts
@@ -1,6 +1,18 @@
-import { LocationResponseDto } from "@/types/location/location";
+import {
+  LocationCreateRequestDto,
+  LocationResponseDto,
+} from "@/types/location/location";
 import { apiClient } from "../client";
 
 export async function getLocations(): Promise<LocationResponseDto[]> {
   return apiClient<LocationResponseDto[]>("/api/v1/locations");
+}
+
+export async function createLocation(
+  request: LocationCreateRequestDto,
+): Promise<LocationResponseDto> {
+  return apiClient<LocationResponseDto>("/api/v1/locations", {
+    method: "POST",
+    body: request,
+  });
 }

16956570d248ddf94f7f2c96d4fc2a81d75358ab
sora-omaru <konggongxia317@gmail.com>
2026-07-30 11:42:57 +0900

    feat:location削除時のエラーレスポンス作成

--- src/lib/api/error/error-location.ts
+++ src/lib/api/error/error-location.ts
@@ -13,3 +13,16 @@ export function getLocationCreateErrorMessage(error: unknown) {
 
   return error.message || defaultCreateErrorMessage;
 }
+
+const defaultDeleteErrorMessage =
+  "保存場所の削除に失敗しました。時間をおいて再度お試しください。";
+
+export function getLocationDeleteErrorMessage(error: unknown) {
+  if (!(error instanceof ApiError)) return defaultDeleteErrorMessage;
+
+  // if (error.code === ErrorCode.ITEM_NOT_FOUND) {
+  //   // return error.message || "商品無し。";
+  // }
+
+  return error.message || defaultDeleteErrorMessage;
+}
